dce3bb5e7ef6669b64aef762daebc955bc00bd12
Estevão <pomerwe@gmail.com>
2019-12-31 15:52:55 -0300

    BackendStarted

---
+++ EsteticaBackend/Db/BaseDbContext.cs
@@ -0,0 +1,42 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EsteticaBackend.Db
+{
+    public class BaseDbContext : DbContext
+    {
+        public BaseDbContext(DbContextOptions options) : base(options){}
+
+        public T AddOrUpdate<T>(T entity) where T : class
+        {
+            if(Entry(entity).State == EntityState.Detached)
+            {
+                Attach(entity);
+            }
+            if(Entry(entity).State != EntityState.Added)
+            {
+                Set<T>().Update(entity);
+            }
+            SaveChanges();
+            return entity;
+        }
+
+        public bool TryDeleteEntity<T>(T entity) where T : class
+        {
+            try
+            {
+                Set<T>().Remove(entity);
+                SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ EsteticaBackend/Db/EsteticaDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EsteticaBackend.Db
+{
+    public class EsteticaDbContext : BaseDbContext
+    {
+        public EsteticaDbContext(DbContextOptions options) : base(options)
+        {
+            base.Database.EnsureCreated();
+        }
+
+        //DbSets
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Configurations
+        }
+    }
+}
