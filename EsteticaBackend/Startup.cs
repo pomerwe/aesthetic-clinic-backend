51136fd39348fb798d357638cb1d7e6719efc81f
Estevão <pomerwe@gmail.com>
2019-12-31 19:20:15 -0300

    Initial DbContext configuration complete

--- EsteticaBackend/Startup.cs
+++ EsteticaBackend/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EsteticaBackend.Db;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -35,13 +37,22 @@ namespace EsteticaBackend
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            
+            services.AddDbContext<EsteticaDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("LocalConnection"))
+                );
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder =>
+                builder.
+                AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+            );
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -52,8 +63,25 @@ namespace EsteticaBackend
                 app.UseHsts();
             }
 
+            CheckForMigration(app);
+
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        private void CheckForMigration(IApplicationBuilder app)
+        {
+            try
+            {
+                using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    scope.ServiceProvider.GetService<EsteticaDbContext>().Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
